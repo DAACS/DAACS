112ee67e6693be63f3df0ced7fc2ea566c35a4e2
Added header for item results <JBryer@excelsior.edu>
2018-09-27 15:07:15 -0400

    Added theme files.

---
+++ build.R
@@ -0,0 +1,11 @@
+if(!require(blogdown)) {
+	devtools::install_github('rstudio/blogdown')
+	library(blogdown)
+}
+
+# Build site
+blogdown::build_site()
+
+# Serve site
+blogdown::serve_site()
+blogdown::stop_server()
